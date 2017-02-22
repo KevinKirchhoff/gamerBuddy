c2f9011cc0df622ee0693d3d87e6c7e810b0c4a0
Kevin Kirchhoff <kevinkirchhoff@Kevins-MacBook-Pro-2.local>
2017-02-22 12:24:54 -0700

    updated template

---
+++ header.php
@@ -0,0 +1,23 @@
+<html>
+	<?php ?>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<link rel="favicon" type="image/x-icon" href="favicon.ico" />
+        <header>
+	<div class ="navBar">
+	<div>	<button class="signUp" id="signUp">Sign Up!</button> </div>
+	<div>	<button class="login" id="Login">Login</button> </div>
+	</div>
+	</header>
+	<body>
+		<div>
+			<div>
+				<img class="logo" src="img/gblogo160.png" />
+			</div>
+		</div>
+		<div class="banner-container">
+			<div id"bannerMain">
+				<img class="banner" src="img/gamerBuddybanner3.png" />
+			</div>
+		</div>
+	</body>
+</html>
