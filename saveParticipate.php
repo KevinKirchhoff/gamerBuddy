90d0a7643da24ddcd9b178f7e76180488a96d01b
Kevin Kirchhoff <KevinKirchhoff@u.boisestate.edu>
2017-04-02 21:39:02 -0600

    small change

--- saveParticipate.php
+++ saveParticipate.php
@@ -3,12 +3,12 @@
 require_once 'dao.php';
 
 $dao=new Dao();
-echo "here";
+
 $game= htmlentities($_POST['game']);
 $console= htmlentities($_POST['console']);
 $age= htmlentities($_POST['age']);
 $note= htmlentities($_POST['note']);
 $dao->save($game,$console,$age,$note);
 
-header("Location:search.php");
+header('Location:search.php');
 ?>
